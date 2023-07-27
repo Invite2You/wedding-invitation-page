45af76ff24cf752b2d029b48a4de267c486033d4
Invite2You <138107964+Invite2You@users.noreply.github.com>
2023-07-27 12:56:32 +0300

    Added supabase

--- החתונה_של_איתן_והודיה/form-submission-handler.js
+++ החתונה_של_איתן_והודיה/form-submission-handler.js
@@ -140,3 +140,61 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+const { createClient } = supabase;
+supabase = createClient(
+  "https://wziiguihgxuxzmuwqmdm.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6aWlndWloZ3h1eHptdXdxbWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAyOTY3NTgsImV4cCI6MjAwNTg3Mjc1OH0._Oo28KpI4Xwk6HCziWdrYMg-Q2h15lkiYhwhgtkRlBo",
+  { debug: true }
+);
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", async function (event) {
+  event.preventDefault();
+  // Get the selected "Coming" option
+  const comingOptions = document.querySelectorAll('input[name="Coming"]');
+  let selectedComingOption = "no"; // Default value
+  comingOptions.forEach((option) => {
+    if (option.checked) {
+      selectedComingOption = option.value;
+    }
+  });
+  // Function to extract the event name from the URL
+  function extractEventNameFromURL() {
+    const url = new URL(window.location.href);
+    const path = url.pathname;
+    const parts = path.split("/");
+    const eventName = parts[parts.length - 1];
+    return eventName;
+  }
+  // Get the selected "Side" option
+  const sideOptions = document.querySelectorAll('input[name="Side"]');
+  let selectedSideOption = null; // Default value as null
+  sideOptions.forEach((option) => {
+    if (option.checked) {
+      selectedSideOption = option.value;
+    }
+  });
+  // Get form reference after the click event
+  const form = document.querySelector("#myForm");
+  const formInputs = form.querySelectorAll("input, button");
+  let submition = {};
+  formInputs.forEach((element) => {
+    const { value, name, type } = element;
+    if (type !== "radio" && value) {
+      submition[name] = value;
+    }
+  });
+
+  // Add the selected radio button values to the submition object
+  submition["Coming"] = selectedComingOption;
+  submition["Side"] = selectedSideOption;
+  // console.log("Form Submission Data:", submition);
+  // Extract the table name from the URL
+  const tableName = extractEventNameFromURL();
+
+  const { error } = await supabase
+    .from(tableName)
+    .insert([submition], { returning: "minimal" });
+  if (error) {
+    alert("There was an error please try again");
+  }
+});
